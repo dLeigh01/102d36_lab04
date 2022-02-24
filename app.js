46525e7f3d967344faacd2c5ed3c640079871712
Dana Huffman <dana.huffman@yahoo.com>
2022-02-23 17:35:10 -0800

    fixed the or statement issues and added greeting based on time

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 let userName = prompt("Can you tell me your name?");
 
-if (userName == "Hexx" || "Kassie" || "Josh" || "Joshua") {
+if (userName == "Hexx" || userName == "Kassie" || userName == "Josh" || userName == "Joshua") {
     document.write("Thanks for checking in, " + userName + "!");
-} else if (userName == "no" | "null") {
+} else if (userName == "no" || userName == null) {
     document.write("Smart. Trust no one.");
-} else if (userName == "Nick" | "Nicholas") {
+} else if (userName == "Nick" || userName == "Nicholas") {
     document.write("Listen, Niko, I know you love the cat but you need to understand what he truly is.");
 } else {
     document.write("Now listen to me, this is very important, " + userName + ".");
--- app2.js
+++ app2.js
@@ -1,6 +1,6 @@
 var isHeCute = prompt("Now a more important question, do you believe cats are cute?");
 
-if (isHeCute == "yes" || "Yes") {
+if (isHeCute == "yes" || isHeCute == "Yes") {
     document.write("I'm sorry to inform you that you are sorely mistaken.");
 } else {
     document.write("Good, you're smart. Smarter than the rest.")
