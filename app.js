3a4d01ae70a1d248f0b365008fc92fba5bec5254
Dana Huffman <dana.huffman@yahoo.com>
2022-02-23 17:14:46 -0800

    added app2 and app3 to prompt user for whether they like cats and change message based on system time

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
-let userName = prompt("Can you tell me your name?")
+let userName = prompt("Can you tell me your name?");
 
-if (userName == "Hexx" | "Kassie" | "Josh" | "Joshua") {
+if (userName == "Hexx" || "Kassie" || "Josh" || "Joshua") {
     document.write("Thanks for checking in, " + userName + "!");
-} else if (userName == "no") {
-    document.write("Smart. Trust no one.")
+} else if (userName == "no" | "null") {
+    document.write("Smart. Trust no one.");
+} else if (userName == "Nick" | "Nicholas") {
+    document.write("Listen, Niko, I know you love the cat but you need to understand what he truly is.");
 } else {
     document.write("Now listen to me, this is very important, " + userName + ".");
 }
---
+++ app2.js
@@ -0,0 +1,7 @@
+var isHeCute = prompt("Now a more important question, do you believe cats are cute?");
+
+if (isHeCute == "yes" || "Yes") {
+    document.write("I'm sorry to inform you that you are sorely mistaken.");
+} else {
+    document.write("Good, you're smart. Smarter than the rest.")
+}
